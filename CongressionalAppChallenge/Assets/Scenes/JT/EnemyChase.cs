ff580e42d66afb528c2d703f6775af115b4d0037
Jeremiah Templeton <19.jeremiah.templeto@ksd.org>
2018-08-30 09:10:27 -0700

    Prototype Stuff -JT

---
+++ CongressionalAppChallenge/Assets/Scenes/JT/EnemyChase.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyChase : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject target;
+	public float chaseSpeed = 2.0f;
+	private bool home = true;
+	private Vector3 homePos;
+
+	private Vector2 moveDirection;
+
+	public float maxMoveDistance = 10.0f;
+	public float chaseTriggerDistance = 1.0f;
+
+	void Start () {
+		homePos = transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 playerPosition = target.transform.position;
+		moveDirection = new Vector2 (0 - transform.position.x, 0 - transform.position.y);
+
+        moveDirection.Normalize();
+        home = false;
+        GetComponent<Rigidbody2D>().velocity = moveDirection * chaseSpeed;
+	}
+    public void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Base")
+        {
+            Debug.Log("foo");
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ CongressionalAppChallenge/Assets/Scenes/JT/SpawnerControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerControl : MonoBehaviour {
+    public GameObject SpawnerTop;
+    public GameObject SpawnerLeft;
+    public GameObject SpawnerRight;
+    public GameObject SpawnerBot;
+    public GameObject Enemy1;
+    public GameObject Enemy2;
+    public GameObject Enemy3;
+    public GameObject Enemy4;
+    public float SpawnTime;
+    private int d1;
+	// Use this for initialization
+	void Start () {
+        SpawnTime = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SpawnTime += Time.deltaTime;
+		if (SpawnTime >= 3)
+        {
+            d1 = Random.Range(0, 5);
+            if (d1 == 1)
+            {
+                Instantiate(Enemy1, SpawnerTop.transform.position, Quaternion.identity);
+            }
+            if (d1 == 2)
+            {
+                Instantiate(Enemy2, SpawnerLeft.transform.position, Quaternion.identity);
+            }
+            if (d1 == 3)
+            {
+                Instantiate(Enemy3, SpawnerRight.transform.position, Quaternion.identity);
+            }
+            if (d1 == 4)
+            {
+                Instantiate(Enemy4, SpawnerBot.transform.position, Quaternion.identity);
+            }
+            SpawnTime = 0;
+        }
+	}
+}
