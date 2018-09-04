d61fd6a4aac61e4666a47a02d8f435d6fc50dbee
Jeremiah Templeton <19.jeremiah.templeto@ksd.org>
2018-09-04 10:29:18 -0700

    Fading Screen and UI Improvements -JT

---
+++ CongressionalAppChallenge/Assets/LevelChanger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour {
+
+    public Animator animator;
+
+    public int LevelToLoad;
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void FadeToLevel (int levelIndex)
+    {
+        LevelToLoad = levelIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(LevelToLoad);
+    }
+    public void Play()
+    {
+        FadeToLevel(1);
+    }
+    public void QuitMenu()
+    {
+        FadeToLevel(0);
+        Time.timeScale = 1;
+    }
+}
