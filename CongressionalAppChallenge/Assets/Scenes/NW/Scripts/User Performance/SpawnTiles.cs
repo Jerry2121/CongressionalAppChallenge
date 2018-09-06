fa002a3701217b8c6b6d44b567ecf005a0e30055
Nathan Weis <17.nathan.weis@ksd.org>
2018-09-06 10:24:02 -0700

    NW_Push_9.06

--- CongressionalAppChallenge/Assets/Scenes/NW/Scripts/User Performance/SpawnTiles.cs
+++ CongressionalAppChallenge/Assets/Scenes/NW/Scripts/User Performance/SpawnTiles.cs
@@ -6,33 +6,34 @@ public class SpawnTiles : MonoBehaviour {
 
     public int y_size;
     public int x_size;
-
-    public GameObject tilePrefab;
-    public GameObject townHallTilePrefab;
+    bool townHallSpawned = false;
+    public GameObject emptyTilePrefab;
+    //public GameObject townHallParent;
+    public GameObject townHallPrefab;
 
     private void Start()
     {
+        
         GetComponent<Transform>().position = new Vector3(- x_size / 2 + 0.5f, - y_size / 2 + 0.5f, 0);
 
-        for (int iy = 0; iy < y_size; iy++)
+        for (int iy = -y_size/2; iy < y_size/2; iy++)
         {
-            for (int ix = 0; ix < x_size; ix++)
+            for (int ix = -x_size/2; ix < x_size/2; ix++)
             {
-                if (ix == x_size / 2 && iy == y_size / 2)
+                GameObject tilePrefab = 
+                Instantiate(emptyTilePrefab, GetComponentInParent<Transform>());
+                tilePrefab.transform.position = new Vector3(ix, iy, 0);
+                tilePrefab.name = "Tile(" + ix + ", " + iy + ")";
+                tilePrefab.tag = "EmptyTile";
+                tilePrefab.GetComponent<Tile_Scripts>().infoHub = GameObject.Find("InfoHub");
+                if (ix == 0 && iy == 0 && !townHallSpawned)
                 {
-                    Instantiate(townHallTilePrefab, GetComponentInParent<Transform>());
-                    townHallTilePrefab.transform.position = new Vector3(ix, iy, 0);
-                    townHallTilePrefab.name = "TownHallTile";
+                    tilePrefab.GetComponent<Tile_Scripts>().SpawnTownHall(ix, iy);
+                    Debug.Log(tilePrefab.name);
+                    townHallSpawned = true;
                 }
-
-                else
-                {
-                    Instantiate(tilePrefab, GetComponentInParent<Transform>());
-                    tilePrefab.transform.position = new Vector3(ix, iy, 0);
-                    tilePrefab.name = "Tile(" + ix + ", " + iy + ")";
-                    tilePrefab.GetComponent<Tile_Scripts>().infoHub = GameObject.Find("InfoHub");
-                }
-            }            
+            }
+                        
         }
     }
 }
