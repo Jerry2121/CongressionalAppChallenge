02221bcf9dd12a2a7359ed4f5a402de090763687
Jeremiah Templeton <19.jeremiah.templeto@ksd.org>
2018-09-12 09:18:58 -0700

    Android support

--- CongressionalAppChallenge/Assets/Scenes/JT/Scripts/MainMenuButtons.cs
+++ CongressionalAppChallenge/Assets/Scenes/JT/Scripts/MainMenuButtons.cs
@@ -16,7 +16,7 @@ public class MainMenuButtons : MonoBehaviour {
 	}
     public void StartB ()
     {
-        SceneManager.LoadScene("MainGame");
+        SceneManager.LoadScene("JT");
     }
     public void QuitA ()
     {
