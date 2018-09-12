78ef22e6212176a95d028ccc0c53e15e073692a0
SwordOfWrynn <bubba.wiens@gmail.com>
2018-09-12 10:31:44 -0700

    Added Swordsman

---
+++ CongressionalAppChallenge/Assets/Scenes/AW/Misc/ScreenResolution.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class ScreenResolution : MonoBehaviour {
+    
+    private PixelPerfectCamera pixelPerfectCamera;
+
+	// Use this for initialization
+	void Start () {
+        pixelPerfectCamera = gameObject.GetComponent<PixelPerfectCamera>();
+
+        Debug.Log(Screen.currentResolution.width);
+        Debug.Log(Screen.currentResolution.height);
+
+        pixelPerfectCamera.refResolutionX = Screen.currentResolution.width / 2;
+        pixelPerfectCamera.refResolutionY = Screen.currentResolution.height / 2;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
