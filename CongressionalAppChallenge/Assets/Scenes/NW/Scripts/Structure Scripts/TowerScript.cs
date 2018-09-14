848e3534543c03ae9de9376eeeb16a6d8a1a6265
Nathan Weis <17.nathan.weis@ksd.org>
2018-09-14 10:31:12 -0700

    small push

--- CongressionalAppChallenge/Assets/Scenes/NW/Scripts/Structure Scripts/TowerScript.cs
+++ CongressionalAppChallenge/Assets/Scenes/NW/Scripts/Structure Scripts/TowerScript.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class TowerScript : MonoBehaviour {
 
-	
+    private List<GameObject> enemiesInRange;
+
+    private GameObject towerTarget;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemies")
+        {
+            enemiesInRange.Add(collision.gameObject);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemies")
+        {
+            enemiesInRange.Remove(collision.gameObject);
+        }
+    }
+
+    void Update()
+    {
+        towerTarget = enemiesInRange[0];
+    }
 }
