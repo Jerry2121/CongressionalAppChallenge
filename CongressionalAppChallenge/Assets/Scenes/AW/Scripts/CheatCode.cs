84dcc18c24b384ce2659e0bf6b4e88e3178f0efc
SwordOfWrynn <bubba.wiens@gmail.com>
2018-10-02 10:27:49 -0700

    Foo

---
+++ CongressionalAppChallenge/Assets/Scenes/AW/Scripts/CheatCode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatCode : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.KeypadMultiply))
+        {
+            Resources();
+        }
+	}
+    public void Resources()
+    {
+        gameObject.GetComponent<GameManagerScript>().woodAcquired = 999;
+        gameObject.GetComponent<GameManagerScript>().steelAcquired = 999;
+        gameObject.GetComponent<GameManagerScript>().stoneAcquired = 999;
+        gameObject.GetComponent<GameManagerScript>().oreAcquired = 999;
+    }
+}
