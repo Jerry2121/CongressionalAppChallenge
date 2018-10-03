fe6c6ebe5e97fd036ef6513b130edf4457ea6e43
SwordOfWrynn <bubba.wiens@gmail.com>
2018-10-03 08:36:40 -0700

    Added a save/load script for the highest score

---
+++ CongressionalAppChallenge/Assets/Scenes/AW/Scripts/SaveLoadHighestScore.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SaveLoadHighestScore : MonoBehaviour
+{
+    public int highScore = 0;
+
+    //Save the player info out to a binary file
+    public void Save(int _highScore)
+    {
+        highScore = _highScore;
+
+        BinaryFormatter bf = new BinaryFormatter();
+
+        FileStream file = File.Open(Application.persistentDataPath + "/HighScore.dat", FileMode.OpenOrCreate);
+        HighScoreInfo myInfo = new HighScoreInfo();
+
+        //put what ever you're saving as myInfo.whatever
+        myInfo.highScore = highScore;
+        bf.Serialize(file, myInfo);
+        file.Close();
+    }
+    public int Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/HighScore.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/HighScore.dat", FileMode.Open);
+            HighScoreInfo myLoadedInfo = (HighScoreInfo)bf.Deserialize(file);
+            highScore = myLoadedInfo.highScore;
+        }
+
+        return highScore;
+    }
+}
+[System.Serializable]
+public class HighScoreInfo
+{
+    public int highScore;
+}
