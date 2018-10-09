8e90fc54d77034e28d83999c4fb86f0a78db08ff
Nathan Weis <17.nathan.weis@ksd.org>
2018-10-09 08:25:08 -0700

    blah

--- CongressionalAppChallenge/Assets/Scenes/NW/A(star)Test/Unit.cs
+++ CongressionalAppChallenge/Assets/Scenes/NW/A(star)Test/Unit.cs
@@ -6,15 +6,24 @@ public class Unit : MonoBehaviour
 {
 
     public Transform target;
+    public Transform townBase;
     public float speed = 20;
     Vector3[] path;
     int targetIndex;
 
     private void Start()
     {
+        townBase = target;
         PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
     }
-
+    private void Update()
+    {
+        if(target == null)
+        {
+            target = townBase;
+            PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+        }
+    }
     public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
     {
         if (pathSuccessful)
@@ -23,6 +32,21 @@ public class Unit : MonoBehaviour
             targetIndex = 0;
             StopCoroutine("FollowPath");
             StartCoroutine("FollowPath");
+        }
+
+        else if (!pathSuccessful)
+        {
+            GameObject[] structures = GameObject.FindGameObjectsWithTag("Structure");
+            float dist = 1000;
+            for(int i = 0; i < structures.Length; i++)
+            {
+                if((structures[i].transform.position - transform.position).magnitude < dist)
+                {
+                    target = structures[i].transform;
+                    dist = (target.transform.position - transform.position).magnitude;
+                }
+
+            }
         }
     }
 
